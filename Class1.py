1c783581fdab16127167e269536df9fca25cb7ba
Shaheertura <72249358+Shaheertura@users.noreply.github.com>
2021-09-12 21:44:12 +0400

    Add files via upload

---
+++ Class1.py
@@ -0,0 +1,6 @@
+print("Shaheer \n Tura")
+print("Shaheer \t Tura")
+print("Shaheer Said,\"I want to learn Python\"")
+print('I like Rumi\'s poem')
+print("I like Rumi's poem")
+print("Hello World!")
