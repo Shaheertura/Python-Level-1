3553505ada6715507b43f5d4965121387444c84f
Shaheertura <72249358+Shaheertura@users.noreply.github.com>
2021-10-31 17:36:23 +0400

    Add files via upload

---
+++ Class12.py
@@ -0,0 +1,16 @@
+'''for i in range(1,5):
+    for j in range(1,5):
+        if(j>=5):
+            print("*",end=" ")
+        else:
+            print("",end=" ")
+    print()'''
+
+n=int(input("Enter the number: "))
+import math
+sq = math.sqrt(n)
+if((sq*sq)==n)
+    print("Perfect Square!")
+else:
+    print("Not Perfect")
+            
