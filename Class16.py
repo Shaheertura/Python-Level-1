a28628892dc98c7148b01d05d1869be2f353c16c
Shaheertura <72249358+Shaheertura@users.noreply.github.com>
2021-11-07 16:52:59 +0400

    Add files via upload

---
+++ Class16.py
@@ -0,0 +1,17 @@
+"""Functions
+1) In Built
+2) Custom
+Create a function:
+def functionName(parameter):
+    statement 1
+    statement 2
+    return variable
+
+
+num=int(input("Enter the Number: "))
+def mulTab():
+    for i in range(1,11):
+        print(num,"X",i,"=",(num*i))
+mulTab()
+"""
+num=int(input("Enter the Number: "))
