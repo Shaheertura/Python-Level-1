8463c676340d3d048712169b6a6df0c4d97f24c5
Shaheertura <72249358+Shaheertura@users.noreply.github.com>
2022-01-11 19:59:21 +0400

    Update Class1.py

--- Class1.py
+++ Class1.py
@@ -4,3 +4,4 @@ print("Shaheer Said,\"I want to learn Python\"")
 print('I like Rumi\'s poem')
 print("I like Rumi's poem")
 print("Hello World!")
+#44oi21hVbQVEnY1iJZ3E2CJrvN1BbRxfSQyztcrLwpJi2Y5onQHHvU55ogaqgERMVz5yh9YyKojDNUcHHxm6wF7mNucVoou
